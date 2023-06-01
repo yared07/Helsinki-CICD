e97eff2ed68d8916d188266b77fe893633e2955b
Yared Taddese <weldemeskelyared@gmail.com>
2023-06-01 08:56:31 +0300

    heath check added

--- full-stack-open-pokedex/app.js
+++ full-stack-open-pokedex/app.js
@@ -4,6 +4,18 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
+app.get('/version', (req, res) => {
+  res.send('2.7.4')
+})
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/mode', (req, res) => {
+  res.send('producation')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
